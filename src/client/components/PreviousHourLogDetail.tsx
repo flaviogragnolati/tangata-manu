9f00d7e8f642ab71a2f11536ee58ea9f48d73383
flavio <flavio.gragnolati@gmail.com>
2024-07-23 22:31:34 -0300

    feat: added edit to prev hourlog display

--- src/client/components/PreviousHourLogDetail.tsx
+++ src/client/components/PreviousHourLogDetail.tsx
@@ -1,8 +1,12 @@
 import { keys } from 'lodash';
+import { useState } from 'react';
 import { Grid, Typography } from '@mui/material';
 
 import type { HourLogFull } from '~/types';
+import BasicModal from '~/components/BasicModal';
+import EditHourLog from '~/components/user/EditHourLog';
 import SiteHourCard from '~/components/user/SiteHourCard';
+import { type NormalizedHourLogContent } from '~/server/lib/controller/hour.controller';
 
 type Props = {
   hours: HourLogFull;
@@ -10,6 +14,10 @@ type Props = {
   year: number;
 };
 export default function PreviousHourLogDetail({ hours, year, month }: Props) {
+  const [open, setOpen] = useState(false);
+  const [selectedLog, setSelectedLog] =
+    useState<NormalizedHourLogContent | null>(null);
+
   const hoursBySite = hours[year]?.[month];
 
   const sites = keys(hoursBySite);
@@ -31,26 +39,46 @@ export default function PreviousHourLogDetail({ hours, year, month }: Props) {
     );
 
   return (
-    <Grid container xs={12}>
-      <Grid item xs={12}>
-        <Typography variant="h6" gutterBottom>
-          Horas cargadas previamente
-        </Typography>
+    <>
+      <Grid container xs={12}>
+        <Grid item xs={12}>
+          <Typography variant="h6" gutterBottom>
+            Horas cargadas previamente
+          </Typography>
+        </Grid>
+        {sites.map((siteId) => {
+          const siteLog = hoursBySite[siteId];
+          if (!siteLog) return null;
+          return (
+            <Grid item key={siteId} xs={12} sm={6} md={4}>
+              <SiteHourCard
+                siteLog={siteLog}
+                editable={true}
+                onEdit={() => {
+                  const hourLog = hours[year]![month]![siteId]!;
+                  setSelectedLog(hourLog);
+                  setOpen(true);
+                }}
+              />
+            </Grid>
+          );
+        })}
+        <Grid item xs={12}>
+          <Typography variant="h6" className="mt-2 text-end">
+            Total de horas: {totalHours}
+          </Typography>
+        </Grid>
       </Grid>
-      {sites.map((siteId) => {
-        const siteLog = hoursBySite[siteId];
-        if (!siteLog) return null;
-        return (
-          <Grid item key={siteId} xs={12} sm={6} md={4}>
-            <SiteHourCard siteLog={siteLog} />
-          </Grid>
-        );
-      })}
-      <Grid item xs={12}>
-        <Typography variant="h6" className="mt-2 text-end">
-          Total de horas: {totalHours}
-        </Typography>
-      </Grid>
-    </Grid>
+      <BasicModal
+        title="Editar horas"
+        dialogProps={{
+          open,
+          onClose: () => setOpen(false),
+          fullScreen: true,
+        }}
+      >
+        <EditHourLog hourLog={selectedLog} setOpen={setOpen} />
+      </BasicModal>
+    </>
   );
 }
