61f08c887817bf523970fa0750420deab8ee7ae8
flavio <flavio.gragnolati@gmail.com>
2024-08-14 23:05:40 -0300

    updt html lang to ES

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`font-sans ${roboto.className}`}>
+    <html lang="es" className={`font-sans ${roboto.className}`}>
       <body>
         <TRPCReactProvider>
           <AppRouterCacheProvider options={{ enableCssLayer: true }}>
